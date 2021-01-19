8938520106603dbd1e1adb3d82d68bfa6c62e3d0
Maksim Iakovishin <maksim_iakovishin@epam.com>
2021-01-19 16:05:27 +0300

    add new branch "dev" and comment

--- src/main/java/com/parking_project/parking/ParkingApplication.java
+++ src/main/java/com/parking_project/parking/ParkingApplication.java
@@ -3,6 +3,9 @@ package com.parking_project.parking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Maksim Iakovishin
+ */
 @SpringBootApplication
 public class ParkingApplication {
 
