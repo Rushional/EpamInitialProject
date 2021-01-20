2831ec8a693b0be2d3b63f615ee1ea976c1ebdd6
EvgeniiChuchkalov <Evgenii_Chuchkalov@epam.com>
2021-01-20 19:46:48 +0300

    Revert useless change and check account detection My previous commits weren't identified properly, so it was hard to tell who the author of the commit was. I'm hoping to fix that with this commit

--- src/main/java/com/parking_project/parking/entity/Car.java
+++ src/main/java/com/parking_project/parking/entity/Car.java
@@ -1,8 +1,4 @@
 package com.parking_project.parking.entity;
 
-// I've added a comment to make another change and check if Idea can push
-// after I've "showed it" that it's indeed possible to do by hand
-// I'm hoping that after showing it that man is still superior will hur it's feelings and actually make it work.
-// I am actually optimistic about this method!
 public class Car {
 }
