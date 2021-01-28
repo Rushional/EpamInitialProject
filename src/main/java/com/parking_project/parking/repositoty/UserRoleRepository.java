818a903d917eb479921eebed2c26c932f964b060
Maksim Iakovishin <maksim_iakovishin@epam.com>
2021-01-28 21:57:34 +0300

    Add Repository annotations

--- src/main/java/com/parking_project/parking/repositoty/UserRoleRepository.java
+++ src/main/java/com/parking_project/parking/repositoty/UserRoleRepository.java
@@ -2,6 +2,8 @@ package com.parking_project.parking.repositoty;
 
 import com.parking_project.parking.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 }
