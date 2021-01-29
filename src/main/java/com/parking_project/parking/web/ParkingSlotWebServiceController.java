4bfbf84a9163b89974c62520ebfdc5f2c03847bd
Maksim Iakovishin <maksim_iakovishin@epam.com>
2021-01-29 13:43:49 +0300

    Add ParkingSlotWebServiceController

--- src/main/java/com/parking_project/parking/web/ParkingSlotWebServiceController.java
+++ src/main/java/com/parking_project/parking/web/ParkingSlotWebServiceController.java
@@ -4,13 +4,14 @@ import com.parking_project.parking.business.service.BookingService;
 import com.parking_project.parking.data.entity.ParkingSlot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("api/availableSlots")
 public class ParkingSlotWebServiceController {
     private final BookingService bookingService;
@@ -20,8 +21,9 @@ public class ParkingSlotWebServiceController {
         this.bookingService = bookingService;
     }
     @GetMapping
-    public  List<ParkingSlot> getAllSlots() {
+    public  String getAllSlots(Model model) {
         List<ParkingSlot> availableSlots = this.bookingService.getAvailableSlots();
-        return availableSlots;
+        model.addAttribute("availableSlots", availableSlots);
+        return "availableSlots";
     }
 }
