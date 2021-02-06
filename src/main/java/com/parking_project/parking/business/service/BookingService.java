663abf5eb61faac42940b71a514391ad7b814705
EvgeniiChuchkalov <Evgenii_Chuchkalov@epam.com>
2021-02-06 03:00:04 +0300

    Refactored BookingServices, just made the code clearer 1) Removed irrelevant "this." 2) Removed irrelevant fields (and also, consequently, constructor arguments) 3) Made a ParkingSlotsComparator class to use instead of creating the same anonymous class twice

--- src/main/java/com/parking_project/parking/business/service/BookingService.java
+++ src/main/java/com/parking_project/parking/business/service/BookingService.java
@@ -2,11 +2,7 @@ package com.parking_project.parking.business.service;
 
 import com.parking_project.parking.data.entity.ParkingSlot;
 import com.parking_project.parking.data.entity.StatusType;
-import com.parking_project.parking.data.repositoty.CarRepository;
-import com.parking_project.parking.data.repositoty.CustomerRepository;
 import com.parking_project.parking.data.repositoty.ParkingSlotRepository;
-import com.parking_project.parking.data.repositoty.ReservationRepository;
-import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,46 +12,34 @@ import java.util.List;
 
 @Service
 public class BookingService {
-    private final CarRepository carRepository;
-    private final CustomerRepository customerRepository;
     private final ParkingSlotRepository parkingSlotRepository;
-    private final ReservationRepository reservationRepository;
 
     @Autowired
-    public BookingService(CarRepository carRepository, CustomerRepository customerRepository,
-                          ParkingSlotRepository parkingSlotRepository, ReservationRepository reservationRepository) {
-        this.carRepository = carRepository;
-        this.customerRepository = customerRepository;
+    public BookingService(ParkingSlotRepository parkingSlotRepository) {
         this.parkingSlotRepository = parkingSlotRepository;
-        this.reservationRepository = reservationRepository;
     }
 
     public List<ParkingSlot> getAllSlots() {
-        Iterable<ParkingSlot> slots = this.parkingSlotRepository.findAll();
+        Iterable<ParkingSlot> slots = parkingSlotRepository.findAll();
         List<ParkingSlot> slotsList = new ArrayList<>();
         slots.forEach(slotsList::add);
-        slotsList.sort(new Comparator<ParkingSlot>() {
-            @Override
-            public int compare(ParkingSlot parkingSlot1, ParkingSlot parkingSlot2) {
-                return parkingSlot1.getId().compareTo(parkingSlot2.getId());
-            }
-        });
-
+        slotsList.sort(new ParkingSlotsComparator());
         return slotsList;
     }
 
     public List<ParkingSlot> getAvailableSlots() {
-        Iterable<ParkingSlot> slots = this.parkingSlotRepository.findParkingSlotsByStatus(StatusType.FREE);
+        Iterable<ParkingSlot> slots = parkingSlotRepository.findParkingSlotsByStatus(StatusType.FREE);
         List<ParkingSlot> available = new ArrayList<>();
         slots.forEach(available::add);
-
-        available.sort(new Comparator<ParkingSlot>() {
-            @Override
-            public int compare(ParkingSlot parkingSlot1, ParkingSlot parkingSlot2) {
-                return parkingSlot1.getId().compareTo(parkingSlot2.getId());
-            }
-        });
+        available.sort(new ParkingSlotsComparator());
         return available;
+    }
+
+    private class ParkingSlotsComparator implements Comparator<ParkingSlot> {
+        @Override
+        public int compare(ParkingSlot parkingSlot1, ParkingSlot parkingSlot2) {
+            return parkingSlot1.getId().compareTo(parkingSlot2.getId());
+        }
     }
 
 }
