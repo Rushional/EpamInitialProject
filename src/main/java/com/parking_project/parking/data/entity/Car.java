569f2c81cb8c9559cb90b70c5c2da34b0492ef59
EvgeniiChuchkalov <Evgenii_Chuchkalov@epam.com>
2021-02-07 08:50:18 +0300

    Remove comments from Car They are now useless, and the code is better off!

--- src/main/java/com/parking_project/parking/data/entity/Car.java
+++ src/main/java/com/parking_project/parking/data/entity/Car.java
@@ -1,6 +1,5 @@
 package com.parking_project.parking.data.entity;
 
-// Not sure if that's the right import - I totally might be using wrong annotations
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,11 +15,6 @@ import java.util.List;
 @Getter
 public class Car {
     @Id
-//    TODO: Will we have a problem with id auto generation?
-//     I mean, it might want to generate ID's automatically, and we don't want that
-//    Well, if it's easy to fix, we'll fix it.
-//    If it's at least somewhat complicated to fix, it's better to just add a column of numerical id's.
-//    And if there isn't a problem, then why am I still writing this WHERE'S THE PR BUTTON?!
     @Column(name = "license_plate", length = 16, nullable = false, unique = true)
     private String licensePlate;
 
@@ -29,8 +23,6 @@ public class Car {
 
     @OneToMany(mappedBy="car")
     private List<Reservation> reservations = new ArrayList<>();
-
-//    TODO: do we need equals and hashCode?
 
 // I haven't added brand and model columns because I don't think we need them
 }
